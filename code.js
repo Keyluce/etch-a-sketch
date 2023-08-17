910078f7dd4baf8d6668e48cfefe217df2d1f47f
KeyLuce <86408122+Keyluce@users.noreply.github.com>
2023-08-17 22:13:04 +0530

    Add changeGridSize function

--- code.js
+++ code.js
@@ -1,12 +1,13 @@
 const container = document.querySelector('.container');
-for (let i = 0; i < 16; i++)
+let size = 16;
+for (let i = 0; i < size; i++)
 {
     let columnDiv = document.createElement('div');
 
     columnDiv.style.display = 'flex';
     columnDiv.style.flex = '1';
     container.appendChild(columnDiv);
-    for (let j = 0; j < 16; j++)
+    for (let j = 0; j < size; j++)
     {
         let rowDiv = document.createElement('div');
         rowDiv.style.flex = '1'
@@ -28,5 +29,17 @@ function changeBGColor(e)
 function changeGridSize(e)
 {
     let gridSize = prompt("Enter grid size (Just one side) : ");
+    if (gridSize > 100)
+    {
+        alert("Sorry, that's not possible. Enter a value below 100.");
+        return;
+    }
+    let columnBlock = document.querySelectorAll('.container > div');
+    for (const column of columnBlock)
+    {
+        container.removeChild(column);
+    }
+
     
 }
+
