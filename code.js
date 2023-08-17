c00cdbec53c1f8aae28a7c3f1ef0a71bfd73a11f
KeyLuce <86408122+Keyluce@users.noreply.github.com>
2023-08-17 20:55:51 +0530

    Add flex = 1 instead of 6.25% to accomodate the individual divs

--- code.js
+++ code.js
@@ -2,17 +2,24 @@ const container = document.querySelector('.container');
 for (let i = 0; i < 16; i++)
 {
     let columnDiv = document.createElement('div');
-    columnDiv.style.height = '6.25%';
+
     columnDiv.style.display = 'flex';
+    columnDiv.style.flex = '1';
     container.appendChild(columnDiv);
     columnDiv.style.border = '2px solid black';
     for (let j = 0; j < 16; j++)
     {
         let rowDiv = document.createElement('div');
-        rowDiv.style.width = '6.25%';
+        rowDiv.style.flex = '1'
         columnDiv.appendChild(rowDiv);
         rowDiv.style.border = '2px solid yellow';
+        rowDiv.addEventListener('mouseenter', changeBGColor)
+
     }
 
 }
 
+function changeBGColor(e)
+{
+   this.style.backgroundColor = 'red';
+}
