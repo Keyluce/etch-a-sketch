dd3e6084d88cbeef53dd5bd48587e55dbf44f1ac
KeyLuce <86408122+Keyluce@users.noreply.github.com>
2023-08-17 22:36:28 +0530

    Add makeGrid function and move initial creation of grid to function call

--- code.js
+++ code.js
@@ -1,45 +1,47 @@
-const container = document.querySelector('.container');
-let size = 16;
-for (let i = 0; i < size; i++)
-{
-    let columnDiv = document.createElement('div');
-
-    columnDiv.style.display = 'flex';
-    columnDiv.style.flex = '1';
-    container.appendChild(columnDiv);
-    for (let j = 0; j < size; j++)
-    {
-        let rowDiv = document.createElement('div');
-        rowDiv.style.flex = '1'
-        columnDiv.appendChild(rowDiv);
-        rowDiv.style.border = '2px solid black';
-        rowDiv.addEventListener('mouseenter', changeBGColor)
-    }
-
+function changeBGColor() {
+    this.style.backgroundColor = 'orange';
 }
-
-const button = document.querySelector('button');
-button.addEventListener('click', changeGridSize);
-
-function changeBGColor(e)
-{
-   this.style.backgroundColor = 'orange';
-}
-
-function changeGridSize(e)
-{
+function changeGridSize() {
     let gridSize = prompt("Enter grid size (Just one side) : ");
-    if (gridSize > 100)
-    {
+    if (gridSize > 100) {
         alert("Sorry, that's not possible. Enter a value below 100.");
         return;
     }
     let columnBlock = document.querySelectorAll('.container > div');
-    for (const column of columnBlock)
-    {
+    for (const column of columnBlock) {
         container.removeChild(column);
     }
-
-    
+    makeGrid(gridSize);
 }
+function makeGrid(size) {
+    for (let i = 0; i < size; i++) {
+        let columnDiv = document.createElement('div');
+
+        columnDiv.style.display = 'flex';
+        columnDiv.style.flex = '1';
+        container.appendChild(columnDiv);
+        for (let j = 0; j < size; j++) {
+            let rowDiv = document.createElement('div');
+            rowDiv.style.flex = '1'
+            columnDiv.appendChild(rowDiv);
+
+            rowDiv.addEventListener('mouseenter', changeBGColor)
+
+
+        }
+    }
+}
+
+const container = document.querySelector('.container');
+let size = 16;
+makeGrid(size);
+
+const button = document.querySelector('button');
+button.addEventListener('click', changeGridSize);
+
+
+
+
+
+
 
